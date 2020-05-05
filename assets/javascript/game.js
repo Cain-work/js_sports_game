f37bc41b9b1128474f65247f509ff77cc829752d
Tony <r.tonycain@gmail.com>
2020-05-04 23:59:16 -0400

    adding numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,31 @@
 const teamOneShootButton = document.querySelector ('#teamone-shoot-button') 
 const teamtwoShootButton = document.querySelector ('#teamtwo-shoot-button')
-const resetButton = document.querySelector ('reset-button')
-let numResets = document.querySelector('num-resets')
-let teamoneNumgoals = document.querySelector ('teamone-numgoals')
-let teamoneNumshoots = document.querySelector('teamone-numshoots')
-let teamtwoNumgoals = document.querySelector('teamtwo-numgoals')
-let teamtwoNumShoots = document.querySelector('teamtwo-numshoots')
+const resetButton = document.querySelector ('#reset-button')
+let numResets = document.querySelector('#num-resets')
+let teamoneNumgoals = document.querySelector ('#teamone-numgoals')
+let teamoneNumshoots = document.querySelector('#teamone-numshoots')
+let teamtwoNumgoals = document.querySelector('#teamtwo-numgoals')
+let teamtwoNumShoots = document.querySelector('#teamtwo-numshoots')
 
+resetButton.addEventListener ("click", function() {
+numResets = numResets + 1
+teamoneNumshoots = teamoneNumshoots + 0
+teamtwoNumShoots = teamtwoNumShoots + 0
 
+console.log ('+ button clicked')
+
+})
+teamOneShootButton.addEventListener ("click", function() { 
+    teamoneNumshoots = teamoneNumshoots + 1
+    teamoneNumgoals = (math.random)
+
+    console.log ('+ button1 clicked')
+    
+    })
+ teamtwoShootButton.addEventListener ("click", function() {
+     teamtwoNumShoots = teamtwoNumShoots + 1
+     teamtwoNumgoals = (math.random)
+
+        console.log ('+ button2 clicked')
+        
+        })    
