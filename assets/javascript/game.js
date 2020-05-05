5b832e5d03c6209b720c9721f66c92b82d428489
Tony <r.tonycain@gmail.com>
2020-05-04 22:18:42 -0400

    defind

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+const teamOneShootButton = document.querySelector ('#teamone-shoot-button') 
+const teamtwoShootButton = document.querySelector ('#teamtwo-shoot-button')
+const resetButton = document.querySelector ('reset-button')
+let numResets = document.querySelector('num-resets')
+let teamoneNumgoals = document.querySelector ('teamone-numgoals')
+let teamoneNumshoots = document.querySelector('teamone-numshoots')
+let teamtwoNumgoals = document.querySelector('teamtwo-numgoals')
+let teamtwoNumShoots = document.querySelector('teamtwo-numshoots')
+
+
