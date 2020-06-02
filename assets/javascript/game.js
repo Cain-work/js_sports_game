1ab12f1a0d9ca437a3a3db386db5849af80af115
Tony Cain <r.tonycain@gmail.com>
2020-06-02 11:26:20 -0400

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,29 +3,39 @@ const teamtwoShootButton = document.querySelector ('#teamtwo-shoot-button')
 const resetButton = document.querySelector ('#reset-button')
 let numResets = document.querySelector('#num-resets')
 let teamoneNumgoals = document.querySelector ('#teamone-numgoals')
-let teamoneNumshoots = document.querySelector('#teamone-numshoots')
+let teamoneNumshots = document.querySelector('#teamone-numshots')
 let teamtwoNumgoals = document.querySelector('#teamtwo-numgoals')
-let teamtwoNumShoots = document.querySelector('#teamtwo-numshoots')
+let teamtwoNumShots = document.querySelector('#teamtwo-numshots')
 
 resetButton.addEventListener ("click", function() {
-numResets = numResets + 1
-teamoneNumshoots = teamoneNumshoots + 0
-teamtwoNumShoots = teamtwoNumShoots + 0
+
+teamoneNumshots.innerHTML = 0
+teamtwoNumShots.innerHTML = 0
+teamoneNumgoals.innerHTML = 0
+teamtwoNumgoals.innerHTML = 0
+numResets.innerHTML = Number(numResets.innerHTML) + 1
 
 console.log ('+ button clicked')
 
 })
 teamOneShootButton.addEventListener ("click", function() { 
-    teamoneNumshoots = teamoneNumshoots + 1
-    teamoneNumgoals = (math.random)
-
+    teamoneNumshots.innerHTML = Number(teamoneNumshots.innerHTML) + 1
+    
+    if (Math.random() * 100 < 80) {
+        teamoneNumgoals.innerHTML = Number(teamoneNumgoals.innerHTML) + 1
+        }
+       
+        
     console.log ('+ button1 clicked')
     
     })
  teamtwoShootButton.addEventListener ("click", function() {
-     teamtwoNumShoots = teamtwoNumShoots + 1
-     teamtwoNumgoals = (math.random)
-
+     teamtwoNumShots.innerHTML = Number (teamtwoNumShots.innerHTML) + 1
+     
+     if (Math.random() * 100 < 80) {
+        teamtwoNumgoals.innerHTML = Number(teamtwoNumgoals.innerHTML) + 1
+        }
+        
         console.log ('+ button2 clicked')
         
         })    
